757ad527add29d1690e766af5b538e1692847dc3
Mahfuj <mdmahfuj.cse@gmail.com>
2026-01-25 23:26:47 +0600

    add jS

---
+++ script.js
@@ -0,0 +1,9 @@
+// DOM Elements
+const animatedBox = document.getElementById('animatedBox');
+const animationPreview = document.getElementById('animationPreview');
+const codeOutput = document.getElementById('codeOutput');
+const copyCodeBtn = document.getElementById('copyCodeBtn');
+const exportBtn = document.getElementById('exportBtn');
+const playBtn = document.getElementById('playBtn');
+const pauseBtn = document.getElementById('pauseBtn');
+const resetBtn = document.getElementById('resetBtn');
